0e97ab7d2fbd8df5097b7a85f7b85776b0c307aa
Vicky Gupta <vickyguptaa7@gmail.com>
2023-07-11 21:39:06 +0530

    solved matrix med probs

---
+++ 45DaysChallenge/Matrix/Medium/Max_rectangle.cpp
@@ -0,0 +1,74 @@
+class Solution
+{
+public:
+    vector<int> nextSmallestLeft(vector<int> &arr)
+    {
+        vector<int> left;
+        stack<int> st;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            while (!st.empty() && arr[st.top()] >= arr[i])
+            {
+                st.pop();
+            }
+            if (st.empty())
+                left.push_back(-1);
+            else
+                left.push_back(st.top());
+            st.push(i);
+        }
+        return left;
+    }
+
+    vector<int> nextSmallestRight(vector<int> &arr)
+    {
+        vector<int> right;
+        stack<int> st;
+        for (int i = (int)arr.size() - 1; i >= 0; i--)
+        {
+            while (!st.empty() && arr[st.top()] >= arr[i])
+            {
+                st.pop();
+            }
+            if (st.empty())
+                right.push_back(arr.size());
+            else
+                right.push_back(st.top());
+            st.push(i);
+        }
+        reverse(right.begin(), right.end());
+        return right;
+    }
+
+    int maxAreaHistogram(vector<int> &arr)
+    {
+        vector<int> leftSmall = nextSmallestLeft(arr);
+        vector<int> rightSmall = nextSmallestRight(arr);
+        int area = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            area = max(area, (rightSmall[i] - leftSmall[i] - 1) * arr[i]);
+        }
+        return area;
+    }
+
+    int maxArea(int M[MAX][MAX], int n, int m)
+    {
+        int mxArea = 0;
+        vector<int> arr(m, 0);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (!M[i][j])
+                {
+                    arr[j] = M[i][j];
+                }
+                else
+                    arr[j] += M[i][j];
+            }
+            mxArea = max(mxArea, maxAreaHistogram(arr));
+        }
+        return mxArea;
+    }
+};
--- 45DaysChallenge/Matrix/readme.md
+++ 45DaysChallenge/Matrix/readme.md
@@ -1,6 +1,7 @@
-| Sr No | Difficulty | Day | Problem                                                                                             | Solution                                         |
-| ----- | ---------- | --- | --------------------------------------------------------------------------------------------------- | ------------------------------------------------ |
+| Sr No | Difficulty | Day | Problem                                                                                                | Solution                                            |
+| ----- | ---------- | --- | ------------------------------------------------------------------------------------------------------ | --------------------------------------------------- |
 | 1     | Medium     | 8   | [Spiral Matrix](https://leetcode.com/problems/spiral-matrix/)                                          | [Solution](./Medium/Spiral_Matrix.cpp)              |
 | 2     | Medium     | 8   | [Rotate Image](https://leetcode.com/problems/rotate-image/)                                            | [Solution](./Medium/Rotate_Image.cpp)               |
 | 3     | Medium     | 8   | [Find the number of islands](https://practice.geeksforgeeks.org/problems/find-the-number-of-islands/1) | [Solution](./Medium/Find_the_number_of_islands.cpp) |
-| 4     | Medium     | 8   | []()                                                                                                | [Solution]()                                        |
+| 4     | Medium     | 8   | [Max Area rectangle*](https://practice.geeksforgeeks.org/problems/max-rectangle/1)                           | [Solution](./Medium/Max_rectangle.cpp)              |
+| 5     | Medium     | 8   | [Surrounded Regions\*](https://leetcode.com/problems/surrounded-regions/)                              | [Solution](./Medium/Surrounded_Regions.cpp)                                        |
