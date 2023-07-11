6098058ce110307609673c369d70b21ebd0917bb
Vicky Gupta <vickyguptaa7@gmail.com>
2023-07-11 20:11:51 +0530

    solved medium matrix probs

---
+++ 45DaysChallenge/Matrix/Medium/Find_the_number_of_islands.cpp
@@ -0,0 +1,40 @@
+
+class Solution
+{
+
+    vector<int> moveX = {1, -1, 0, 0, 1, -1, 1, -1};
+    vector<int> moveY = {0, 0, -1, 1, 1, 1, -1, -1};
+
+public:
+    // Function to find the number of islands.
+    void dfs(int x, int y, vector<vector<char>> &grid, vector<vector<bool>> &visit)
+    {
+        int n = grid.size(), m = grid[0].size();
+        visit[x][y] = true;
+        for (int i = 0; i < 8; i++)
+        {
+            int _x = x + moveX[i], _y = y + moveY[i];
+            if (_x >= n || _y >= m || _x < 0 || _y < 0 || visit[_x][_y] || grid[_x][_y] == '0')
+                continue;
+            dfs(_x, _y, grid, visit);
+        }
+    }
+
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<bool>> visit(n, vector<bool>(m, 0));
+        int island = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (visit[i][j] || grid[i][j] == '0')
+                    continue;
+                island++;
+                dfs(i, j, grid, visit);
+            }
+        }
+        return island;
+    }
+};
---
+++ 45DaysChallenge/Matrix/readme.md
@@ -0,0 +1,6 @@
+| Sr No | Difficulty | Day | Problem                                                                                             | Solution                                         |
+| ----- | ---------- | --- | --------------------------------------------------------------------------------------------------- | ------------------------------------------------ |
+| 1     | Medium     | 8   | [Spiral Matrix](https://leetcode.com/problems/spiral-matrix/)                                          | [Solution](./Medium/Spiral_Matrix.cpp)              |
+| 2     | Medium     | 8   | [Rotate Image](https://leetcode.com/problems/rotate-image/)                                            | [Solution](./Medium/Rotate_Image.cpp)               |
+| 3     | Medium     | 8   | [Find the number of islands](https://practice.geeksforgeeks.org/problems/find-the-number-of-islands/1) | [Solution](./Medium/Find_the_number_of_islands.cpp) |
+| 4     | Medium     | 8   | []()                                                                                                | [Solution]()                                        |
