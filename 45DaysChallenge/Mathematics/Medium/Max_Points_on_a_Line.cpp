2cc850d7e3f27efe570b507cae141e64614bcbaf
Vicky Gupta <vickyguptaa7@gmail.com>
2023-07-13 10:26:20 +0530

    solved mathematics medium questions

---
+++ 45DaysChallenge/Mathematics/Medium/Max_Points_on_a_Line.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int maxPoints(vector<vector<int>> &points)
+    {
+        int mxPoints = 1;
+        for (int i = 0; i < points.size(); i++)
+        {
+            map<pair<int, int>, int> freq;
+            for (int j = i + 1; j < points.size(); j++)
+            {
+                int _x = points[i][0] - points[j][0];
+                int _y = points[i][1] - points[j][1];
+                int gcd = __gcd(_x, _y);
+                freq[{_x / gcd, _y / gcd}]++;
+            }
+            for (auto x : freq)
+                mxPoints = max(mxPoints, x.second + 1);
+        }
+        return mxPoints;
+    }
+};
---
+++ 45DaysChallenge/Mathematics/Medium/Valid_Square.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    bool validSquare(vector<int> &p1, vector<int> &p2, vector<int> &p3, vector<int> &p4)
+    {
+        vector<pair<int, int>> coord;
+        coord.push_back({p1[0], p1[1]});
+        coord.push_back({p2[0], p2[1]});
+        coord.push_back({p3[0], p3[1]});
+        coord.push_back({p4[0], p4[1]});
+        map<int, int> freq;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (i == j || coord[i] == coord[j])
+                    continue;
+                int x = coord[i].first - coord[j].first;
+                int y = coord[i].second - coord[j].second;
+                freq[x * x + y * y]++;
+            }
+        }
+        if (freq.size() == 2 && freq.begin()->second == 8 && freq.rbegin()->second == 4)
+            return true;
+        return false;
+    }
+};
